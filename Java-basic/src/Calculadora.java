b85e01c5b9d637343384e3ac80513107ac48f3bd
Santos Matheus <matheus.santos1012@ettec.sp.gov.br>
2025-04-17 22:24:04 -0300

    Calculadora-java

---
+++ Java-basic/src/Calculadora.java
@@ -0,0 +1,14 @@
+
+
+
+public class Calculadora{
+   /**
+   * @param numeroUm
+   * @param numeroDois
+   * @return
+    */
+
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
